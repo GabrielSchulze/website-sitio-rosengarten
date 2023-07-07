1b29c01e5d26ebb8ec59d27ed7ed3903a37397b1
GabrielSchulze <gabrielschulze@hotmail.com>
2023-07-06 23:36:18 -0300

    Atualização index.html, imagens, js e css

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-  <script>
-    $(document).ready(function() {
-      $('#imageCarousel').carousel({
-        interval: false
-      });
-
-      $('.carousel-control-prev').click(function() {
-        $('#imageCarousel').carousel('prev');
-      });
-
-      $('.carousel-control-next').click(function() {
-        $('#imageCarousel').carousel('next');
-      });
+$(document).ready(function () {
+    $('#imageCarousel').carousel({
+        interval: 3000
     });
-  </script>
+
+    $('#avaliacoesCarousel').carousel({
+        interval: 35000
+    });
+
+    $('#fotosCarousel').carousel({
+        interval: 3000
+    });
+
+    $('#imageCarousel .carousel-control-prev').click(function () {
+        $('#imageCarousel').carousel('prev');
+    });
+
+    $('#imageCarousel .carousel-control-next').click(function () {
+        $('#imageCarousel').carousel('next');
+    });
+
+    $('#avaliacoesCarousel .carousel-control-prev').click(function () {
+        $('#avaliacoesCarousel').carousel('prev');
+    });
+
+    $('#avaliacoesCarousel .carousel-control-next').click(function () {
+        $('#avaliacoesCarousel').carousel('next');
+    });
+
+    $('#imageCarousel .carousel-control-prev').click(function () {
+        $('#fotosCarousel').carousel('prev');
+    });
+
+    $('#imageCarousel .carousel-control-next').click(function () {
+        $('#fotosCarousel').carousel('next');
+    });
+});
